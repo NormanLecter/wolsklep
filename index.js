87e391786751f6479bb72bd3bf9cc1be17ca9fa8
Łukasz <lukasz.maksymilian.wolniak@gmail.com>
2017-11-15 16:24:31 +0100

    DEVELOP - test connection with SQL Server
    test connection with SQL Server using mssql ^2.1.8

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('test');
+var sql = require('mssql');
+
+var dbConfig  = {
+  server : 'localhost\\WOLNIAK',
+  database : 'NORTHWND',
+  user  : 'wolniak', //TODO: hash function
+  password : 'wolniak', //TODO: hash function
+  port: 1433 //TODO: hash function
+};
+
+function  testDb(){
+  var conn = new sql.Connection(dbConfig);
+  var req = new sql.Request(conn);
+
+  conn.connect(function (err) { //TODO: lambda function
+    if(err){
+      console.log(err);
+    }
+    req.query("SELECT * FROM Customers", function (err, data) { //TODO: LAMBDA FUNCTION
+      if(err){
+        console.log(err);
+      }
+      else{
+        console.log(data);
+      }
+
+    })
+  })
+};
+
+testDb();
