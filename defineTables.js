c07fb1c6fa874b3a4cf7b4203d061f3c32c68714
Łukasz <lukasz.maksymilian.wolniak@gmail.com>
2017-12-13 08:34:47 +0100

    Sequelize
    more services and functionality

--- defineTables.js
+++ defineTables.js
@@ -1,35 +1,49 @@
 const Sequelize = require('sequelize');
 const dbConnection = require('./dbConnection.js');
 
-//connection to the database
+// connection to the database
 const sequelize = dbConnection.sequelize;
 
+// model of SPRZET
 exports.Sprzet = sequelize.define('SPRZET',{
   IdSprzetu: {
     type: Sequelize.INTEGER,
     allowNull: false,
     primaryKey: true,
-    autoIncrement: true
+    autoIncrement: true,
+    unique : true
   },
   Marka: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      is: /^([A-Z0-9]+).*$/
+    }
   },
   Model: {
     type: Sequelize.STRING,
-    allowNull: true
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      is: /^([A-Z0-9]+).*$/
+    }
   },
   Typ: {
     type: Sequelize.STRING,
-    allowNull: true
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      is: /^([A-Z]+).*$/
+    }
   },
   PN: {
     type: Sequelize.STRING,
-    allowNull: true
+    allowNull: false
   },
   SN: {
     type: Sequelize.STRING,
-    allowNull: true
+    allowNull: false
   },
   Uwagi: {
     type: Sequelize.STRING,
--- index.js
+++ index.js
@@ -1,61 +1,93 @@
 const Sequelize = require('sequelize');
 const dbConnection = require('./dbConnection.js');
-const defineSprzet = require('./defineTables.js');
+const defineTables = require('./defineTables.js');
 
-//connection to the database
+// connection to the database
 const sequelize = dbConnection.sequelize;
 
-// Models of tables in WOOLSKLEP database
-// TODO: Rest of table + new sprzet, update data, conditions of cohesion
-const Sprzet = defineSprzet.Sprzet;
+// Models of tables in WOLSKLEP database
+// TODO: Rest of table, update data, conditions of cohesion
+const Sprzet = defineTables.Sprzet;
+
+// variable to check state of operations
+let checkState = 1;
 
 //function with return promise as collected data from database (value)
-function findAllSprzet(){
-  return new Promise(resolve => {
-    Sprzet.findAll().then((sprzet) => {
-        let sprzetArr = new Array(sprzet.length);
-        for(let  i = 0; i<sprzet.length; i++){
-              sprzetArr[i] = sprzet[i];
-        }
-        resolve(sprzetArr);
+function addToSprzet(){
+    Sprzet.create({
+      //IdSprzetu is autoIncrement, TODO: fine works ???
+      Marka: 'Tooomek',
+      Model: 'Lol',
+      Typ: 'Router',
+      PN: 0,
+      SN: 0,
+      Uwagi: '4x LAN, 2x antena, WiFi'
     })
-    //TODO: number of error to enum;  rest of error services
+    //TODO: number of error to enum; rest of error services
     .catch(Sequelize.DatabaseError, err => {
-      console.log(err);
       if(err.parent.number == 2627){
-        console.log("\nPodane IdSprzetu juz istnieje w bazie!\n")
+        console.log("\nPodane IdSprzetu juz istnieje w bazie!\n");
+        checkState = 0;
       }
       else if(err.parent.number == 245)
-        console.log("\nPodana wartosc nie jest typu Int!\n")
+        console.log("\nPodana wartosc nie jest typu Int!\n");
+        checkState = 0;
       })
     .catch(Sequelize.ValidationError, err => {
+      if(err.errors[0].type == 'Validation error'){
+        //TODO:  Better service for Validation Error for each field
+        console.log("Blad walidacji podczas dodawania sprzetu!");
+        checkState = 0;
+      }
       if(err.errors[0].type == 'notNull Violation'){
         console.log('\nPole nie moze byc null!\n');
+        checkState = 0;
       }
     });
-  });
-};
+    Sprzet.findAll().then((sprzet) => {
+    for(let  i = 0; i<sprzet.length; i++){
+          console.log(sprzet[i]);
+    }
+    })
+}
 
-// authenticate in database, then (promise) call function,
+// sync table, authenticate in database, then (promise) call function,
 //then (promise) close connection and print data
 sequelize
-.authenticate()
-.then(async () => {
-  console.log('Connection has been established successfully.');
-  let sprzetAll = await findAllSprzet();
+.sync()
+.then(() => {
   sequelize
-  .close()
-  .then(() => {
-    console.log('Connection has been close successfully.');
-    // print data
-    for(let i = 0; i < sprzetAll.length; i++){
-      console.log(sprzetAll[i].dataValues);
+  .authenticate()
+  .then(async () => {
+    console.log('Connection has been established successfully.');
+    // TODO: use check with resolve/reject
+    let check = await addToSprzet();
+    sequelize
+    .close()
+    .then(() => {
+      console.log('Connection has been close successfully.');
+      if(checkState == 1){
+        console.log('Data has been added successfully.');
     }
+      else{
+        console.log('Something goes wrong, data has not been added successfully.');
+      }
+    })
+    .catch(err => {
+      console.error('Unable to close connection with the database:', err);
+    });
   })
   .catch(err => {
-    console.error('Unable to close connection with the database:', err);
+    console.error('Unable to connect to the database:', err);
   });
 })
 .catch(err => {
-  console.error('Unable to connect to the database:', err);
+  console.error('Unable to sync with the database:', err);
 });
+
+// TODO: WARUNKI  SPOJNOSCI - BAZY DANYCH, RESZTA TABEL  +  VWALIDACJA,
+// DO  KONCA  SERVER, STRONA GLOWNA - ADMIN, KLIENT, ZAMOW/KUP BEZ KONTA = (LOGOWANIE);
+// PO ZALOGOWANIU WIDOK  NASTEPUJACY =>
+// - ADMIN MOZE : TEST  OK, TEST FAIL, DODAJ NOWE, MODYFIKUJ - TRIGER/PROCEDUR
+// - KLIENT MOZE : KUP lub ZAMOW INNE
+// - GOSC  : KUP BEZ KONTA, ZAMOW BEZ KONTA  lub ZALOZ  KONTO
